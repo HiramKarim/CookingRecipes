864b304da0149cbf8697f7180a44a2a47d9ce95a
Hiram Castro <hiram2027@gmail.com>
2020-10-31 11:45:55 -0700

    Project Configuration
    + Adding folder's Structure
    + Adding Recipe List Table view
    + Adding Table and Search bar to Table List view controller

---
+++ CookingRecipes/CookingRecipes/Views/RecipeCell.swift
@@ -0,0 +1,23 @@
+//
+//  RecipeCell.swift
+//  CookingRecipes
+//
+//  Created by Hiram Castro on 10/31/20.
+//
+
+import UIKit
+
+class RecipeCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
